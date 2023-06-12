de8f3204e70248a5af1e92de133c77de6b2f60eb
Trevor DePew <t.depewmusic@gmail.com>
2023-06-12 14:13:11 -0400

    updated some small layout items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account information updated successfully."
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
